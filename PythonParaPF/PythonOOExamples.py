01d685421b02c37c4f3580d5fb04e951a0c8db54
samuellincoln <samuellincoln@gmail.com>
2023-10-29 21:45:13 -0300

    Add files via upload

---
+++ PythonParaPF/PythonOOExamples.py
@@ -0,0 +1,61 @@
+#public class SerVivo {
+#    Date data_nascimento;
+#    public SerVivo (Date data_nascimento) {
+#        this.data_nascimento = data_nascimento;
+#    }
+#}
+class SerVivo :
+    def __init__ (self, data_nascimento):
+        self.__data_nascimento = data_nascimento
+
+
+#public class Cachorro extends SerVivo{
+class Cachorro (SerVivo) :
+#    private String nome;
+#    private String raca;
+#    public Cachorro(Date data_nascimento, String nome, String raca) {
+#        super(data_nascimento);
+#        this.nome = nome;
+#        this.raca = raca;
+#    }
+    def __init__ (self, data_nascimento, nome, raca):
+        super().__init__ (data_nascimento)
+        self.__nome = nome
+        self.__raca = raca
+#    public String getNome() {
+#        return nome;
+#    }
+    def getNome (self) :
+        return self.__nome
+#    public void setNome(String nome) {
+#        this.nome = nome;
+#    }
+    def setNome (self, nome):
+        self.__nome = nome
+#    public String getraca() {
+#        return raca;
+#    }
+    def getRaca (self):
+        return self.__raca
+#    public void setraca(String raca) {
+#        this.raca = raca;
+#    }
+    def setRaca (self, raca):
+        self.__raca = raca
+#    public String toString () {
+#        String str =
+#                "Cachorro: " + this.nome + "\n"
+#            +   "raca: " + this.raca;
+#        ;
+#        return str;
+#    }
+    def toString (self) :
+        var_str = "Cachorro: " + self.__nome + "\n" + "Raca: " + self.__raca
+        return var_str
+#    public static void main (String [] args) {
+#        Cachorro c = new Cachorro (new Date (), "Shurastey", "Golden Retriever");
+#        System.out.println (c.toString());
+#    }
+c = Cachorro ("29.10.2023", "Shurastey", "Golden Retriever")
+print (c.toString())
+#}
